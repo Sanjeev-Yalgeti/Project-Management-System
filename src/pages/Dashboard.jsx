d6a43a184b734672cc580b5a4f3672bc6fbbe866
Sanjeev Yalgeti <yalgetisanju@gmail.com>
2026-06-25 15:41:52 +0530

    feat: changes to dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 export default function Dashboard(){
 
     return(
-        <div>
-            
+        <div className="bg-[#c9fdd7] h-screen w-full grid grid-cols-5 grid-rows-6 "> 
+                
         </div>
     )
 }

4a20f0870eb29af2c602fb46e21c6aa864485f39
Sanjeev Yalgeti <yalgetisanju@gmail.com>
2026-07-01 21:54:33 +0530

    feat:added MongoDB connection - config and server.js file - added Model:-Company,Task -added Middlewares:-authMiddleware,errorMiddleware

---
+++ backend/server/models/Company.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+const CompanySchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+    },
+    noOfProjects:{
+        type:Number,
+    },
+    desc:{
+        type:String,
+    },
+},{timestamps:true
+})
+export const Company=mongoose.model("Company",CompanySchema)
+
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,2 +1,14 @@
-// import dotenv.config()
-
+import dotenv from 'dotenv'
+import express from 'express'
+import connectDB from './config/db.js'
+dotenv.config()
+const app=express()
+connectDB();
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send("Backend Running ");
+});
+const PORT=process.env.PORT || 5000;
+app.listen(PORT ,()=>{
+    console.log(`server running on ${PORT} `);
+});

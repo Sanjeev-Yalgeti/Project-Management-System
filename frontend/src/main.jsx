49c9e872956cf4de0f146025a0006129599b10be
Sanjeev Yalgeti <yalgetisanju@gmail.com>
2026-07-05 15:20:34 +0530

    "fix:main.jsx "

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ProtectedRoute from './assets/components/ProtectedRoute.jsx'
 import Login from './frontend/Login.jsx'
 createRoot(document.getElementById('root')).render(
-  <ProtectedRoute>
     <StrictMode>
       <Login />
-    </StrictMode>
-  </ProtectedRoute>,
-  
+    </StrictMode>,
 )
